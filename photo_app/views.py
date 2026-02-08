a9c85335a08950fb91f1f8e2527094fd1a24da86
Roy <mutuaroymulinge@gmail.com>
2026-02-08 09:41:49 +0300

    add the register function in photo_app views.py file

--- photo_app/views.py
+++ photo_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        login(request, user)
+        return redirect("home")  # we’ll define this later
+
+    return render(request, "register.html")
