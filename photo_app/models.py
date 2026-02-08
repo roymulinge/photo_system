9e06beebd95575beb64f7936ef69d02e70fde057
Roy <mutuaroymulinge@gmail.com>
2026-02-08 11:35:35 +0300

    add class photo and register in the admin

--- photo_app/models.py
+++ photo_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 # Create your models here.
 class Profile(models.Model):
@@ -25,3 +26,27 @@ class Profile(models.Model):
     def create_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+
+class Photo(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="photos"
+    )
+    title =models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="photos/")
+    tags = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Comma-separated tags"
+    )
+    liked_by = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="liked_photos",
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
