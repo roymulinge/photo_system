60d17f69f3839eb51c48690dab5eec290473f544
Roy <mutuaroymulinge@gmail.com>
2026-02-08 11:03:46 +0300

    add automatic usercreation in models when  a new user is created

--- photo_app/models.py
+++ photo_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OnToOneField(
         User,
         on_delete=models.CASCADE
@@ -15,3 +19,9 @@ class profile(models.Model):
     )
     def __str__(self):
         return self.user.username
+    
+    # Automatically create a Profile when a new User is created
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
