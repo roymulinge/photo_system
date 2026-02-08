851a3db133670cb5d3d6b968685baab7683b3ac5
Roy <mutuaroymulinge@gmail.com>
2026-02-08 15:02:03 +0300

    add  media_url and root, login and logout redirect-urls

--- photo_project/settings.py
+++ photo_project/settings.py
@@ -120,10 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
-# Media files
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+ 
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
