83a7cd9449913c01fcbd9eb7d543dacef17b261a
Roy <mutuaroymulinge@gmail.com>
2026-02-08 10:59:04 +0300

    add class profile in models.py

--- photo_app/models.py
+++ photo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class profile(models.Model):
+    user = models.OnToOneField(
+        User,
+        on_delete=models.CASCADE
+
+    )
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(
+        upload_to="profiles/", 
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return self.user.username
